539c5bdd459f753ea8f3b9c667c157719c040d20
Pedro Bellotti <bellottipcpedro@gmail.com>
2021-03-24 11:17:08 -0300

    Fazendo upload na pasta correta

--- server.py
+++ server.py
@@ -4,9 +4,7 @@ from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 from datetime import datetime
 app = Flask(__name__)
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-target = os.path.join(APP_ROOT, '/Videos')
-app.config['UPLOAD_FOLDER'] = target
+app.config['UPLOAD_FOLDER'] = './Videos/'
 
 @app.route('/')
 def uploader_file():
@@ -17,8 +15,10 @@ def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       dt = datetime.now()
-      filename = dt.strftime("%d-%b-%YT%H:%M:%S")
-      f.save(secure_filename(filename))
+      timestamp = dt.strftime("%d-%b-%YT%H:%M:%S")
+      name = f.filename.rsplit('.', 1)[0].lower()
+      filename = secure_filename(name + "-" + timestamp)
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
       return 'Arquivo enviado com sucesso!'
 
 if __name__ == '__main__':
